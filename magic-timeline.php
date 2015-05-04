8b6d803a683e1e04be8c57770482e05ee317536a
bryce meyer <bryce@Rupert.local>
2015-05-04 12:34:51 -0500

    chanded include to init word wrap

--- magic-timeline.php
+++ magic-timeline.php
@@ -108,6 +108,6 @@ MagicTimeline_i18n_init();
 // If it is successful, continue with initialization for this plugin
 if (MagicTimeline_PhpVersionCheck()) {
     // Only load and run the init function if we know PHP version can parse it
-    include_once('magic-timeline_init.php');
+    include_once(__DIR__ . '../word-wrap/word-wrap.php');
     WordWrap_init(__DIR__);
 }
